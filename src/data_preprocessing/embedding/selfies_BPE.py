b95e7aa1d2e9b1b1fa907d0d93e386c12dafdce0
Charliefff <charlie88531@gmail.com>
2025-02-12 20:08:51 +0800

    tokenizer file

--- src/data_preprocessing/embedding/selfies_BPE.py
+++ src/data_preprocessing/embedding/selfies_BPE.py
@@ -7,16 +7,16 @@ def build_tokenizer(path, output_path):
     with open(path, "r") as f:
         selfies_data = [line.strip() for line in f.readlines()]
         
-    tokenizer.train(selfies_data, max_vocab_size=3000, 
+    tokenizer.train(selfies_data, max_vocab_size=10000, 
                     min_freq_for_merge=50, 
                     save_checkpoint=True, 
-                    checkpoint_path="./checkpoints_kinase_inhibitor")
+                    checkpoint_path="./checkpoints")
     tokenizer.save_vocabulary(output_path)
     return
 
 
 def main():
-    path = '/data/tzeshinchen/RLMOE_kinase_inhibitor/dataset/inhibitor_selfies.txt'
+    path = '/data/tzeshinchen/RLMOE_kinase_inhibitor/dataset/extracted_file.txt'
     output_path = "trained_vocabulary.json"
 
     print("Vocab file found, building tokenizer")
